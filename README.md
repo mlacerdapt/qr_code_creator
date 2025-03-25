f0c43b54e489801a71009b068e65d568b2b55cbe
Marcelo Lacerda <marcelo.melo.lacerda@gmail.com>
2025-03-25 12:54:37 +0000

    criação QR Code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # qr_code_creator
  Criação de QR Code
+
+
+ https://pypi.org/project/qrcode/
---
+++ create_qrcode_advanced.py
@@ -0,0 +1,15 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+#cores RGB
+
+img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
